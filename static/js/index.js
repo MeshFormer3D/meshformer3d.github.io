1ba1188caede017028fbf2c7d998568b45f329ae
MeshFormer3D <>
2024-06-10 15:30:06 +0800

    add comp videos

--- static/js/index.js
+++ static/js/index.js
@@ -14,65 +14,73 @@ function preloadInterpolationImages() {
 
 function setInterpolationImage(i) {
   var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
+  image.ondragstart = function () { return false; };
+  image.oncontextmenu = function () { return false; };
   $('#interpolation-image-wrapper').empty().append(image);
 }
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
 
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    loop: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  }
+
+  // Initialize all div with carousel class
+  var carousels = bulmaCarousel.attach('.carousel', options);
+
+  // Loop on each carousel initialized
+  for (var i = 0; i < carousels.length; i++) {
+    // Add listener to  event
+    carousels[i].on('before:show', state => {
+      console.log(state);
     });
+  }
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
+  // Access to bulmaCarousel instance of an element
+  var element = document.querySelector('#my-element');
+  if (element && element.bulmaCarousel) {
+    // bulmaCarousel instance is available as element.bulmaCarousel
+    element.bulmaCarousel.on('before-show', function (state) {
+      console.log(state);
+    });
+  }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+  /*var player = document.getElementById('interpolation-video');
+  player.addEventListener('loadedmetadata', function() {
+    $('#interpolation-slider').on('input', function(event) {
+      console.log(this.value, player.duration);
+      player.currentTime = player.duration / 100 * this.value;
+    })
+  }, false);*/
+  // preloadInterpolationImages();
 
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
+  // $('#interpolation-slider').on('input', function(event) {
+  //   setInterpolationImage(this.value);
+  // });
+  // setInterpolationImage(0);
+  // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
+  bulmaSlider.attach();
 
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    // preloadInterpolationImages();
+  $('.tabs li').click(function () {
+    var target = $(this).attr('data-tab');
 
-    // $('#interpolation-slider').on('input', function(event) {
-    //   setInterpolationImage(this.value);
-    // });
-    // setInterpolationImage(0);
-    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    $('.tabs li').removeClass('is-active');
+    $(this).addClass('is-active');
 
-    bulmaSlider.attach();
-
+    $('.content-tab').removeClass('is-active');
+    $('#' + target).addClass('is-active');
+  });
 })
